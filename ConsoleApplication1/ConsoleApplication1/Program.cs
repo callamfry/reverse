f147d6dd01b67a82ed66eeb62028796b744b5fd8
Callamfry <callamfry@aol.com>
2016-06-21 11:34:01 +0100

    source code

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("please enter a word");
+
+
+
+           string input = Console.ReadLine();
+            string reverse = "";
+            int i;
+            for (i = 0; i < input.Length; i++)
+            {
+                reverse = input[i] + reverse;
+            }
+            Console.WriteLine(reverse);
+            Console.ReadLine();
+        }
+    }
+}
